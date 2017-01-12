c642958892f7b932f583da793e463d6c58ca4368
Nihhaar <nihhaar@gmail.com>
2017-01-12 22:46:49 +0000

    Adapt pixel scripts from different sources
    Change-Id: I9d580236c5a2079d2fcb94db0a3df42b40e12440

--- product/fake_pixel.mk
+++ product/fake_pixel.mk
@@ -5,19 +5,15 @@ PRODUCT_PACKAGES := \
 	FakeNexusLayout \
 	PixelSoundResources \
 	Provision \
+	SetupWizard \
 
 PRODUCT_PROPERTY_OVERRIDES := \
 	ro.config.ringtone=Zen.ogg \
 	ro.config.notification_sound=Chime.ogg \
 	ro.config.alarm_alert=Flow.ogg \
 	ro.config.density_override=440 \
+	ro.opa.eligible_device=true \
 	ro.product.name=Pixel XL \
 
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	ro.product.name=Pixel XL \
-
-PRODUCT_PROPERTY_OVERRIDES += \
-	keyguard.no_require_sim=true \
-
-PRODUCT_COPY_FILES := vendor/pixelrom/data/media/bootanimation.zip:system/media/bootanimation.zip:fake_nexus \
-	vendor/pixelrom/data/data-pixel/nexus.xml:system/etc/sysconfig/nexus.xml:fake_nexus \
+PRODUCT_COPY_FILES := vendor/pixelrom/media/bootanimation.zip:system/media/bootanimation.zip:fake_nexus \
+	vendor/pixelrom/data-pixel/nexus.xml:system/etc/sysconfig/nexus.xml:fake_nexus \
