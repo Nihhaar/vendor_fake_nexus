0dbc6dcefefb0f661b705f62c9c3637e86354817
Nihhaar_RC <nihhaar@gmail.com>
2017-01-13 04:02:23 +0530

    Remove unneeded things.

--- product/fake_pixel.mk
+++ product/fake_pixel.mk
@@ -2,7 +2,6 @@ PRODUCT_PACKAGE_OVERLAYS := \
 	vendor/fake_nexus/product/overlay-pixel \
 
 PRODUCT_PACKAGES := \
-	FakeNexusLayout \
 	PixelSoundResources \
 	Provision \
 
@@ -11,13 +10,8 @@ PRODUCT_PROPERTY_OVERRIDES := \
 	ro.config.notification_sound=Chime.ogg \
 	ro.config.alarm_alert=Flow.ogg \
 	ro.config.density_override=440 \
-	ro.product.name=Pixel XL \
-
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-	ro.product.name=Pixel XL \
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	keyguard.no_require_sim=true \
 
-PRODUCT_COPY_FILES := vendor/pixelrom/data/media/bootanimation.zip:system/media/bootanimation.zip:fake_nexus \
-	vendor/pixelrom/data/data-pixel/nexus.xml:system/etc/sysconfig/nexus.xml:fake_nexus \
+PRODUCT_COPY_FILES := vendor/pixelrom/data/media/bootanimation.zip:system/media/bootanimation.zip:fake_nexus
